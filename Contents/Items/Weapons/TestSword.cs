13f99a8f67088d20f565e782b335c5e37b370914
otakuict <otaku.ict@gmail.com>
2025-05-04 19:26:30 +0700

    test sword

---
+++ Contents/Items/Weapons/TestSword.cs
@@ -0,0 +1,35 @@
+using Microsoft.Xna.Framework ;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace test01.Contents.Items.Weapons
+{
+    public class TestSword : ModItem {
+      public override void SetDefaults() {
+			Item.width = 40; // The item texture's width.
+			Item.height = 40; // The item texture's height.
+
+			Item.useStyle = ItemUseStyleID.Swing; // The useStyle of the Item.
+			Item.useTime = 20; // The time span of using the weapon. Remember in terraria, 60 frames is a second.
+			Item.useAnimation = 20; // The time span of the using animation of the weapon, suggest setting it the same as useTime.
+			Item.autoReuse = true; // Whether the weapon can be used more than once automatically by holding the use button.
+
+			Item.DamageType = DamageClass.Melee; // Whether your item is part of the melee class.
+			Item.damage = 50; // The damage your item deals.
+			Item.knockBack = 6; // The force of knockback of the weapon. Maximum is 20
+			Item.crit = 6; // The critical strike chance the weapon has. The player, by default, has a 4% critical strike chance.
+
+			Item.value = Item.buyPrice(gold: 1); // The value of the weapon in copper coins.
+			Item.rare = ItemRarityID.Master;
+			Item.UseSound = SoundID.Item1; // The sound when the weapon is being used.
+		}
+  	public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone) {
+			// Inflict the OnFire debuff for 1 second onto any NPC/Monster that this hits.
+			// 60 frames = 1 second
+			target.AddBuff(BuffID.OnFire, 60);
+		}
+    }
+    
+ }
